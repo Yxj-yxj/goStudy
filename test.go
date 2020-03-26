f15a7dd5b9096a4bf02ad663dd64a7a73bc45841
Yxj-yxj <36444134+Yxj-yxj@users.noreply.github.com>
2020-03-26 20:45:47 +0800

    Add files via upload

---
+++ test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func g() {
+	fmt.Println("Starting the program")
+	panic("A severe error occurred: stopping the program!")
+	fmt.Println("Ending the program")
+}
+
+func protect(g func()) {
+	defer func() {
+		log.Println("done")
+		// Println executes normally even if there is a panic
+		//if err := recover();err!=nil{
+		//	log.Printf("run time panic: %v", err)
+		//}
+	}()
+	log.Println("start")
+	g() //   possible runtime-error
+}
+
+func main()  {
+	protect(g)
+	fmt.Println("program end")
+}
